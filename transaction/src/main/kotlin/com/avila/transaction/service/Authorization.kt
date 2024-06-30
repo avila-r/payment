912d31069c6cdc86bbf78427d3c6759999972db0
avila-r <avila.dev@outlook.com>
2024-06-30 20:03:29 -0300

    feat: add auth service fields

--- transaction/src/main/kotlin/com/avila/transaction/service/Authorization.kt
+++ transaction/src/main/kotlin/com/avila/transaction/service/Authorization.kt
@@ -2,30 +2,39 @@ package com.avila.transaction.service
 
 import com.avila.transaction.model.Transaction
 
+import org.springframework.beans.factory.annotation.Value
+
 import org.springframework.web.client.RestClient
 
 private val client = RestClient.create()
 
+@Value("\${services.authorization.uri}")
+private lateinit var uri: String
+
+@Value("\${services.authorization.endpoints.authorize-transaction}")
+private lateinit var endpoint: String
+
 data class Authorization ( val authorized: Boolean )
 
-fun authorize( transaction: Transaction ): String? { // TODO: Return Result<V, E>
+fun authorize(transaction: Transaction): String? { // TODO: Return Result<V, E>
 
     // TODO: Logging
 
     val authorization =
-        client.get()
-            .uri("") // TODO: Authorization service endpoint
+        client.post()
+            .uri(uri + endpoint)
+            .body(transaction)
             .retrieve()
             .toEntity(Authorization::class.java)
 
     if (authorization.statusCode.isError) {
-        // TODO: Error
+        // TODO: Error: unstable authorization service. Cancel transaction.
     }
 
     val message = authorization.body ?: return null // TODO: Error
 
     if (!message.authorized) {
-        // TODO: Error
+        // TODO: Error: unauthorized transaction
     }
 
     return "authorized"
