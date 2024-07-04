2c3800705d088890e03ad332967451128f501457
avila-r <avila.dev@outlook.com>
2024-07-03 23:10:05 -0300

    fix: enable jpa auditing

--- transaction/src/main/kotlin/com/avila/transaction/TransactionApplication.kt
+++ transaction/src/main/kotlin/com/avila/transaction/TransactionApplication.kt
@@ -2,8 +2,10 @@ package com.avila.transaction
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class TransactionApplication
 
 fun main(args: Array<String>) {
--- transaction/src/main/kotlin/com/avila/transaction/model/Transaction.kt
+++ transaction/src/main/kotlin/com/avila/transaction/model/Transaction.kt
@@ -2,38 +2,40 @@ package com.avila.transaction.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
 import jakarta.persistence.Table
 
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Table(name = "transactions")
+@EntityListeners(AuditingEntityListener::class)
 @Entity class Transaction (
 
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false, unique = true)
     @Id var id: UUID? = null,
 
-    @JoinColumn(name = "payer_id", nullable = false)
+    @Column (name = "payer_id", nullable = false)
     val payer: Long,
 
-    @JoinColumn(name = "payee_id", nullable = false)
+    @Column(name = "payee_id", nullable = false)
     val payee: Long,
 
-    val value: BigDecimal,
+    val value: BigDecimal
 
-    @CreatedDate
-    @Column(nullable = false)
+) {
+    @field:CreatedDate
+    @Column(nullable = false, updatable = false)
     var createdAt: LocalDateTime? = null
-
-)
+}
 
 data class TransactionRequest (
     val payer: Long,
